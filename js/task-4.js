b27f5820de7ccd7822db9644a2b924a1193cfa68
darina-bigos <darinaapp358@gmail.com>
2025-05-18 13:26:03 +0200

    up task 4

--- js/task-4.js
+++ js/task-4.js
@@ -15,17 +15,16 @@ function handleSubmit(event) {
   event.preventDefault();
 
   const elements = event.target.elements;
-
   const info = {
     email: elements.email.value,
     password: elements.password.value,
   };
-  console.log(info);
 
   if (info.email === '' || info.password === '') {
     return alert('All form fields must be filled in');
+  } else {
+    form.reset();
   }
-  form.reset();
 
   console.log(info);
 }
